9c6c04e0c69211f83f417885567030af21a75ca8
dongzoolee <nant0313@gmail.com>
2021-03-28 14:33:34 +0900

    Connect DB With Env Information

--- src/main/java/cloud2/demo/HandleDB/HandleDB.java
+++ src/main/java/cloud2/demo/HandleDB/HandleDB.java
@@ -1,17 +1,32 @@
 package cloud2.demo.HandleDB;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 
+@RestController
 public class HandleDB {
 
+    @Value("${spring.datasource.url}")
+    private String URL;
+
+    @Value("${spring.datasource.username}")
+    private String Username;
+
+    @Value("${spring.datasource.password}")
+    private String Password;
+
+    @Autowired
     public void GetFiles() {
         try {
-
             Connection conn = DriverManager.getConnection(
-                    "",
-                    "",
-                    ""
+                    URL,
+                    Username,
+                    Password
             );
 
             Statement sql = conn.createStatement();
